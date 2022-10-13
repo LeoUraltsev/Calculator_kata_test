5e7b57e9db8a9215ef43f0b095f157844bb472cc
Leo <LeoUtaltsev@gmail.com>
2022-10-13 17:00:06 +0400

    Читаем выражение из консоли, и сразу удаляем все введенные пробелы

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+
+/**
+ *
+ * Создай консольное приложение “Калькулятор”.
+ * Приложение должно читать из консоли введенные пользователем строки, числа, арифметические операции проводимые между
+ * ними и выводить в консоль результат их выполнения.
+ * Реализуй класс Main с методом public static String calc(String input). Метод должен принимать строку с арифметическим
+ * выражением между двумя числами и возвращать строку с результатом их выполнения. Ты можешь добавлять свои импорты,
+ * классы и методы. Добавленные классы не должны иметь модификаторы доступа (public или другие)
+ *
+ */
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Введите выражение: ");
+        String input = scanner.nextLine().replaceAll("\\s+", "");
+
+        System.out.println("Вывод: " + input);
 
     }
 
 
+
+    public static String calc(String input){
+        return "";
+    }
+
 }
+
