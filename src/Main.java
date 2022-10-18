046aa83f30fb1f9f664c874fe79d0fb493543239
Leo <LeoUtaltsev@gmail.com>
2022-10-18 11:33:22 +0400

    Реализован калькулятор арабских цифр.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import java.util.List;
 import java.util.Scanner;
 
 /**
- *
  * Создай консольное приложение “Калькулятор”.
  * Приложение должно читать из консоли введенные пользователем строки, числа, арифметические операции проводимые между
  * ними и выводить в консоль результат их выполнения.
  * Реализуй класс Main с методом public static String calc(String input). Метод должен принимать строку с арифметическим
  * выражением между двумя числами и возвращать строку с результатом их выполнения. Ты можешь добавлять свои импорты,
  * классы и методы. Добавленные классы не должны иметь модификаторы доступа (public или другие)
- *
  */
 
 /**
@@ -31,6 +28,7 @@ import java.util.Scanner;
 public class Main {
 
     private static final String NO_OPERATOR = "OPERATOR IS NULL";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -42,47 +40,83 @@ public class Main {
 
     }
 
-    public static String calc(String input){
+    public static String calc(String input) {
         String operator = getOperator(input);
-        if(operator.equals(NO_OPERATOR)){
+        if (operator.equals(NO_OPERATOR)) {
             //TODO: Выбросить исключение ("Введено неккоректное выражение")
             return "Введено неккоректное выражение";
         }
 
         String[] operands = input.split("[-+/*]");
-        if(operands.length > 2){
+        if (operands.length > 2) {
             //TODO: Выбросить исключение ("Введено неккоректное выражение")
             return "Введено неккоректное выражение";
         }
         System.out.println(operands[0] + operands[1]);
 
-        if(checkByRoman(operands[0]) && checkByRoman(operands[1])){
+        int firstOperand = 0;
+        int secondOperand = 0;
+
+        if (checkByRoman(operands[0]) && checkByRoman(operands[1])) {
             System.out.println("Is roman");
-        }else if (!checkByRoman(operands[0]) && !checkByRoman(operands[1])){
+
+        } else if (!checkByRoman(operands[0]) && !checkByRoman(operands[1])) {
             System.out.println("Is arabic");
-            int firstOperand = 0;
-            int secondOperand = 0;
+
             try {
                 firstOperand = Integer.parseInt(operands[0]);
                 secondOperand = Integer.parseInt(operands[1]);
-            }catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 return "Введено неккоректное выражение " + e;
             }
-            System.out.println(firstOperand + secondOperand);
-        }else {
+
+            if (firstOperand > 0 && secondOperand > 0 && firstOperand <= 10 && secondOperand <= 10) {
+                return calculation(firstOperand, secondOperand, operator);
+            } else {
+                //TODO: Выбросить исключение ("Введено неккоректное выражение")
+                return "Введено неккоректное выражение";
+            }
+
+        } else {
             //TODO: Выбросить исключение ("Введено неккоректное выражение")
             return "Введено неккоректное выражение";
         }
         return "";
     }
 
-    public static boolean checkByRoman(String value){
+    public static String calculation(int firstOperand, int secondOperand, String operator) {
+        int result = 0;
+
+        switch (operator) {
+            case "+": {
+                result = firstOperand + secondOperand;
+                break;
+            }
+            case "-": {
+                result = firstOperand - secondOperand;
+                break;
+            }
+            case "*": {
+                result = firstOperand * secondOperand;
+                break;
+            }
+            case "/": {
+                result = firstOperand / secondOperand;
+                break;
+            }
+        }
+
+        return String.valueOf(result);
+    }
+
+    public static boolean checkByRoman(String value) {
         RomanNumberConverter converter = new RomanNumberConverter();
         return converter.isRomanNumber(value);
     }
-    private static String getOperator(String input){
+
+    private static String getOperator(String input) {
         String operator = NO_OPERATOR;
-        String[] operatorsList = {"+","-","*","/"};
+        String[] operatorsList = {"+", "-", "*", "/"};
 
         for (String s : operatorsList) {
             if (input.contains(s)) {
